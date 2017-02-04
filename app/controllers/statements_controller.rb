6bd0eb207ba9a584b192c48c86b2482e7e871792
macbk <macb88@gmail.com>
2017-02-05 00:50:58 +0100

    Improve statement creation process

--- app/controllers/statements_controller.rb
+++ app/controllers/statements_controller.rb
@@ -1,5 +1,5 @@
 class StatementsController < ApplicationController
-	#before_action :find_statement, only: [:show, :edit, :update, :destroy]
+	before_action :find_statement, only: [:show, :edit, :update, :destroy]
 
 	def show
 	end
@@ -10,9 +10,11 @@ class StatementsController < ApplicationController
 	end
 
 	def create
-		@statement = current_helicopter.statements.build(statement_params)
+		@current_helicopter = Helicopter.find_by(id: session[:selected_helicopter_id])
+		@statement = @current_helicopter.statements.build(statement_params)
 		if @statement.save
-			redirect_to @statement, notice: "Successfully created new statement"
+			flash[:success] = 'MS almost done'
+			redirect_to @statement
 		else
 			render 'new'
 		end
@@ -33,7 +35,7 @@ class StatementsController < ApplicationController
 		params.require(:statement).permit(:sot, :crs)
 	end
 
-	#def find_statement
-	#	@statement = Statement.find(params[:id])
-	#end
+	def find_statement
+		@statement = Statement.find(params[:id])
+	end
 end
