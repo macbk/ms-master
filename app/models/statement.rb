60b125a32b4f550429a311b225469f47bf0ac785
macbk <macb88@gmail.com>
2017-02-22 21:46:22 +0100

    Improve task form validation

--- app/models/statement.rb
+++ app/models/statement.rb
@@ -1,7 +1,7 @@
 class Statement < ApplicationRecord
 	belongs_to :helicopter
 	has_many :tasks, inverse_of: :statement
-	accepts_nested_attributes_for :tasks, reject_if: ->(attrs) { attrs['title'].blank? || attrs['term'].blank? }, allow_destroy: true
+	accepts_nested_attributes_for :tasks, reject_if: :all_blank, allow_destroy: true
 	validates :crs, presence: true, length: {minimum: 12}
 	validates :sot, presence: true
 end
--- app/models/task.rb
+++ app/models/task.rb
@@ -1,3 +1,4 @@
 class Task < ApplicationRecord
   belongs_to :statement
+  validates :title, :term, presence: true
 end
